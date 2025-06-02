9c544adb21441789fd05ad9917ce0d2dad7814fb
Pham Anh Tuan <phamtuan210394@gmail.com>
2025-06-02 16:28:46 +0700

    Layout

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <main>
+            <h1 className="text-4xl font-bold">About Us</h1>
+            <div className="mt-5">About Us and Our Awesome Application</div>
+        </main>
+    );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
+import React from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,12 +25,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <html lang="en">
+      <body className={`${geistSans.className} ${geistMono.className} px-2 md:px-5 dark`}>
+      <header className="text-white font-bold bg-green-900 text-2xl p-2 mb-3 rounded-b-lg shadow-gray-700 shadow-lg flex">
+          <div className="flex flex-grow">
+              <Link href="/">GPT Chat</Link>
+              <Link href="/about" className="ml-5 font-light">
+                  About
+              </Link>
+          </div>
+          <div></div>
+      </header>
+      <div className="flex flex-col md:flex-row">
+          <div className="flex-grow">{children}</div>
+      </div>
       </body>
-    </html>
+      </html>
   );
 }
